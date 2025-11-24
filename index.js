034ddaccfd16d89546775b2ddc7dbf4a86c93634
SN-Digitech-Neeraj <neerajsndigitech1@gmail.com>
2025-11-24 16:26:49 +0530

    All done

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: [ "process.env.FRONTEND_URL", "http://localhost:5173"] || "*" ,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders:['Content-Type', 'Authorization'],
   credentials: true
 }));
 
